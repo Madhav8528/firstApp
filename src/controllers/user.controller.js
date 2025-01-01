56077bed64c5316643ce96677d42f465c94315fd
Madhav <madhavv8528@gmail.com>
2025-01-01 21:41:56 +0530

    db aggregation and routes for new controllers added

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,6 +4,7 @@ import { ApiError } from "../utils/apiError.js"
 import { cloudinaryUpload }  from "../utils/cloudinary.js"
 import { ApiResponse } from "../utils/apiResponse.js"
 import jwt from "jsonwebtoken"
+import mongoose from "mongoose"
 
 const generateAccessAndRefreshToken = async function(userId){
     try { 
@@ -395,6 +396,54 @@ const getUserProfile = asyncHandler(async (req, res) => {
 })
 
 
+const getWatchHistory = asyncHandler(async (req, res) => {
+    const user = await User.aggregate([
+        {
+            $match : {
+                _id : new mongoose.Types.ObjectId(req.user?._id)
+            }
+        },
+        {
+            $lookup : {
+                from : "videos",
+                localField : "watchHistory",
+                foreignField : "_id",
+                as : "watchHistory",
+                pipeline : [
+                    {
+                        $lookup : {
+                            from : "users",
+                            localField : "owner",
+                            foreignField : "_id",
+                            as : "owner",
+                            pipeline : [{
+                                $project : {
+                                    username : 1,
+                                    fullName : 1,
+                                    avatar : 1
+                                }
+                            }]
+                        }
+                    },
+                    {
+                        $addFields : {
+                            owner : {
+                                $first : "$owner"
+                            }
+                        }
+                    }
+                ]
+            }
+        }
+    ])
+
+    return res.status(200)
+    .json(
+        new ApiResponse(200, user[0].watchHistory, "Watch history fetched successfully")
+    )
+
+})
+
 export {registerUser,
         loginUser, 
         logOutUser, 
@@ -404,4 +453,5 @@ export {registerUser,
         updateAccountDetails,
         updateAvatar,
         updateCoverImage,
-        getUserProfile};
+        getUserProfile,
+        getWatchHistory};
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -23,4 +23,12 @@ router.route("/login").post(loginUser);
 //secured routes
 router.route("/logout").post(verifyJwt, logOutUser)
 router.route("/refresh-token").post(refreshAccessToken)
+router.route("/change-current-password").post(verifyJwt,changeCurrentPassword)
+router.route("/get-current-user").get(verifyJwt,getCurrentUser)
+router.route("update-account-details").patch(verifyJwt, updateAccountDetails)
+router.route("/update-avatar").patch(verifyJwt, upload.single("avatar"), updateAvatar)
+router.route("/update-cover-image").patch(verifyJwt, upload.single("coverImage"), updateCoverImage)
+
+router.route("/c/:username").get(verifyJwt, getUserProfile)
+router.route("/watch-history").get(verifyJwt, getWatchHistory)
 export default router;
